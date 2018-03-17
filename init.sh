20054d59d8b679088ce9701b089ae6b9a25babd9
Анна Михайлец <amile@amile-mbp.kvm-router>
2018-03-17 20:36:55 +0300

    add gunicorn conf

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/gunicorn.cong  /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 ﻿sudo /etc/init.d/mysql start﻿
