22cb7dd79fc5c5c27ff115ca9c9d890579ff2b62
Анна Михайлец <amile@amile-mbp.kvm-router>
2018-04-17 14:11:28 +0300

    Add pagination

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -21,7 +21,7 @@ def new(request, *args, **kwargs):
 	except ValueError:
 		raise Http404
 	questions = Question.objects.new()
-	paginator = Paginator(questions, 10)
+	paginator = Paginator(questions, 3)
 	try:
 		page = paginator.page(page)
 	except EmptyPage: 
